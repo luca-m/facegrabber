76075a356c19cd1b5d277d14e7e76907554912dc
Luca M <lucam.ko@gmail.com>
2013-07-24 02:52:06 +0200

    draw detected regions

--- facegrabber.test/src/testDetection.cpp
+++ facegrabber.test/src/testDetection.cpp
@@ -25,7 +25,6 @@ const char win_detail_1[] = "Detail 1";
 char lena[] = "data/lena.tif";
 char video[] = "data/video.avi";
 
-
 int main(int argv, char * args[]) {
 	try {
 		IplImage * image;
@@ -44,51 +43,19 @@ int main(int argv, char * args[]) {
 		cvNamedWindow(win_detail_1, 1);
 
 		while ((image = capture.nextFrame())) {
+
 			IFaceRegion & f = detector.detect(image);
 
-			CvRect * area = f.getFace();
-			if (area != 0) {
+			CvRect * area;
+			if (f.hasFace()) {
+				area = f.getFace();
 				cvRectangle(image, cvPoint(area->x, area->y),
 						cvPoint(area->x + area->width, area->y + area->height),
 						CV_RGB(127, 127, 127), 1, 8, 0);
-
-				/*
-				 int *bbox = (int*) malloc(4 * sizeof(int));
-				 double *landmarks = (double*) malloc(
-				 2 * model->data.options.M * sizeof(double));
-				 IplImage * igray = cvCreateImage(
-				 cvSize(image->width, image->height), IPL_DEPTH_8U, 1);
-				 cvCvtColor(image, igray, CV_RGB2GRAY);
-				 //cvEqualizeHist(igray,igray);
-				 //cvSmooth(igray, igray, CV_GAUSSIAN, 7, 7, 2.6);
-				 bbox[0] = area->x;
-				 bbox[1] = area->y;
-				 bbox[2] = area->x + area->width;
-				 bbox[3] = area->y + area->height;
-				 flandmark_detect(igray, bbox, model, landmarks);
-				 // display landmarks
-				 //cvRectangle(image, cvPoint(bbox[0], bbox[1]),
-				 //		cvPoint(bbox[2], bbox[3]), CV_RGB(255,0,0));
-				 //cvRectangle(image, cvPoint(model->bb[0], model->bb[1]),
-				 //		cvPoint(model->bb[2], model->bb[3]), CV_RGB(0,0,255));
-				 cvCircle(image, cvPoint((int) landmarks[0], (int) landmarks[1]),
-				 3, CV_RGB(0, 0,255), CV_FILLED);
-
-				 cvCircle(image, cvPoint(int(landmarks[2]), int(landmarks[3])),
-				 3, CV_RGB(0,255,0), CV_FILLED);
-				 cvCircle(image, cvPoint(int(landmarks[4]), int(landmarks[5])),
-				 3, CV_RGB(255,0,0), CV_FILLED);
-
-				 //for (int i = 2; i < 2 * model->data.options.M; i += 2) {
-				 //	cvCircle(image,
-				 //			cvPoint(int(landmarks[i]), int(landmarks[i + 1])),
-				 //			3, CV_RGB(255,0,0), CV_FILLED);
-				 //
-				 //}
-				 */
 			}
-			area = f.getEyeL();
-			if (area != 0) {
+
+			if (f.hasEyeL()) {
+				area = f.getEyeL();
 				cvRectangle(image, cvPoint(area->x, area->y),
 						cvPoint(area->x + area->width, area->y + area->height),
 						CV_RGB(255, 127, 127), 1, 8, 0);
@@ -98,81 +65,39 @@ int main(int argv, char * args[]) {
 				cvCvtColor(t, tgray, CV_RGB2GRAY);
 				cvEqualizeHist(tgray, tgray);
 				cvSmooth(tgray, tgray, CV_GAUSSIAN, 7, 7, 2.6);
-				//cvDilate(tgray, tgray,
-				//		cvCreateStructuringElementEx(3, 3, 0, 0,
-				//				CV_SHAPE_ELLIPSE), 1);
-				//cvThreshold(tgray, tgray, 5, 255, CV_THRESH_BINARY_INV);
-				//cvErode(tgray, tgray,
-				//		cvCreateStructuringElementEx(5, 5, 0, 0, CV_SHAPE_RECT),
-				//		1);
-				//cvDilate(tgray, tgray,
-				//		cvCreateStructuringElementEx(5, 5, 0, 0, CV_SHAPE_RECT),
-				//		1);
-				//show_histogram("hist", tgray, "blue");
-				//cvCanny( tgray, tgray, 5, 5*3, 3);
-				//cvErode(tgray, tgray,
-				//		cvCreateStructuringElementEx(3, 3, 0, 0,
-				//				CV_SHAPE_ELLIPSE), 1);
-				//cvDilate(tgray, tgray,
-				//		cvCreateStructuringElementEx(3, 3, 0, 0,
-				//				CV_SHAPE_ELLIPSE), 1);
-				//cvCircle(image,
-				//		cvPoint(cvRound(area->x + area->width/2),
-				//				cvRound(area->y + area->height/2)), 3, CV_RGB(0,255,0),
-				//		-1, 8, 0);
-				CvMemStorage* storage = cvCreateMemStorage(0);
-				CvSeq* circles = cvHoughCircles(tgray, storage,
-						CV_HOUGH_GRADIENT, 1, tgray->height / 4, 10, 10 * 3,
-						sqrt(tgray->width * tgray->height / 20 / 3.14),
-						sqrt(tgray->width * tgray->height / 4 / 3.14));
-				for (int i = 0; i < circles->total; i++) {
-					float* p = (float*) cvGetSeqElem(circles, i);
-					cvCircle(image,
-							cvPoint(cvRound(area->x + p[0]),
-									area->y + cvRound(p[1])), 3,
-							CV_RGB(0,255,0), -1, 8, 0);
-					cvCircle(image,
-							cvPoint(cvRound(area->x + p[0]),
-									cvRound(area->y + p[1])), cvRound(p[2]),
-							CV_RGB(255,0,0), 1, 8, 0);
-					cvCircle(tgray, cvPoint(cvRound(p[0]), cvRound(p[1])), 3,
-					CV_RGB(0,0,255), -1, 8, 0);
-				}
-				/*
-				 cvErode(tgray, tgray,
-				 cvCreateStructuringElementEx(3, 3, 0, 0,
-				 CV_SHAPE_RECT), 8);
-				 cvThreshold(tgray, tgray, 127, 255,
-				 CV_THRESH_BINARY | CV_THRESH_OTSU);
-				 */
-				//cvShowImage(win_detail_1, tgray);
+
 			}
-			area = f.getEyeBrowL();
-			if (area != 0) {
+
+			if (f.hasEyeBrowL()) {
+				area = f.getEyeBrowL();
 				cvRectangle(image, cvPoint(area->x, area->y),
 						cvPoint(area->x + area->width, area->y + area->height),
 						CV_RGB(127, 255, 255), 1, 8, 0);
 			}
-			area = f.getEyeBrowR();
-			if (area != 0) {
+
+			if (f.hasEyeBrowR()) {
+				area = f.getEyeBrowR();
 				cvRectangle(image, cvPoint(area->x, area->y),
 						cvPoint(area->x + area->width, area->y + area->height),
 						CV_RGB(127, 255, 255), 1, 8, 0);
 			}
-			area = f.getEyeR();
-			if (area != 0) {
+
+			if (f.hasEyeR()) {
+				area = f.getEyeR();
 				cvRectangle(image, cvPoint(area->x, area->y),
 						cvPoint(area->x + area->width, area->y + area->height),
 						CV_RGB(127, 127, 127), 1, 8, 0);
 			}
-			area = f.getMouth();
-			if (area != 0) {
+
+			if (f.hasMouth()) {
+				area = f.getMouth();
 				cvRectangle(image, cvPoint(area->x, area->y),
 						cvPoint(area->x + area->width, area->y + area->height),
 						CV_RGB(127, 127, 127), 1, 8, 0);
 			}
-			area = f.getNose();
-			if (area != 0) {
+
+			if (f.hasNose()) {
+				area = f.getNose();
 				cvRectangle(image, cvPoint(area->x, area->y),
 						cvPoint(area->x + area->width, area->y + area->height),
 						CV_RGB(127, 127, 127), 1, 8, 0);
